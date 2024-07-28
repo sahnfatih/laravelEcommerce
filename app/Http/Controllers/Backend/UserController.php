5c8e61b12a3d8aa437aacc543b2cee8a28867754
sahnfatih <sahnfatih49@gmail.com>
2024-07-29 01:12:08 +0300

    user pull +-

--- app/Http/Controllers/Backend/UserController.php
+++ app/Http/Controllers/Backend/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
@@ -14,7 +15,8 @@ class UserController extends Controller
      */
     public function index()
     {
-       return view(view: "backend.users.index");
+        $users = User::all();
+        return view("backend.users.index", ["users" => $users]);
     }
 
     /**
