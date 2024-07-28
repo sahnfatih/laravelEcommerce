744fb98da131a996fd4c28741087e71f139b13f3
sahnfatih <sahnfatih49@gmail.com>
2024-07-28 23:47:57 +0300

    Dashboard+

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource(name: "/Users", controller: UserController::class);
